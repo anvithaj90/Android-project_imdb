53e236fb50609c922b8c5855b58d169ccf750cc2
anvithaj90 <anvithaj90@gmail.com>
2013-02-14 01:34:28 -0500

    Added the java and php to the database

---
+++ h8.php
@@ -0,0 +1,121 @@
+<?php
+$testchk=$_GET['title'];
+$op = preg_replace('!\s+!','', $testchk);
+
+ if (isset($_GET['title']) && ($_GET['title']!="")&& ($op!="") ) 
+{ 
+	   
+	   $textbox1= $_GET['title'];
+	   $textbox11=$textbox1;
+	   $textbox1 = strtolower($textbox1);			## to lower case...
+	   
+	 
+	   $textbox1=str_replace("&", "%26", $textbox1);
+	 	$textbox1=preg_replace('!\s+!', '%20', $textbox1);
+		$selectbox1=$_GET['title_type'];
+		if($selectbox1!='at')
+		$url = 'http://www.imdb.com/search/title?title='.$textbox1.'&title_type='.$selectbox1;
+        else{
+		$selectbox1="All Types";
+		$url = 'http://www.imdb.com/search/title?title='.$textbox1.'&title_type=feature,tv_series,game'; 
+		}
+		
+	}
+else
+{
+	$xop= '<script type="text/javascript">alert("Please enter the title!");window.location.href="http://cs-server.usc.edu:20212/hw6.html"</script>"';
+}
+	
+	
+$str = file_get_contents($url);
+$output = preg_replace('!\s+!', ' ', $str);
+$patt0='#No results#U';
+$first_check=preg_match($patt0,$output,$matches);
+if($first_check==1)
+{
+	$selectbox1=$_GET['title_type'];
+	if($selectbox1=='feature')
+	$selectbox11="feature";
+	else if($selectbox1=='tv_series')
+	$selectbox11="tv_series";
+	else if($selectbox1=='game')
+	$selectbox11="game";
+	else if($selectbox1=='at')
+	$selectbox11="All Types";
+	$xop='<br /><br /><br /><br /><br />Search Result<br />"'.$textbox11.'" of type "'.$selectbox11.'":<br />No Movies found!';
+	
+}
+else
+{
+	if($selectbox1=='feature')
+	$selectbox11="feature";
+	else if($selectbox1=='tv_series')
+	$selectbox11="tv_series";
+	else if($selectbox1=='game')
+	$selectbox11="game";
+	else if($selectbox1=='at' ||$selectbox1=="All Types")
+	$selectbox11="All Types";
+	$patt1='#<table class="results">(.*)</table>#U';
+	preg_match($patt1,$output,$matches);
+	$pieces = explode(" </tr>", $matches[0]);
+	if(count($pieces)-1>5)
+		$counter_sub=5;
+	else
+		$counter_sub=count($pieces)-2;
+	$xop= '<?xml version="1.0"?>';
+	$xop=$xop.'<rsp stat="ok">';
+	$xop=$xop.'<results total="5">';
+	
+	
+	for ($i=1; $i<=$counter_sub; $i++)
+	{	
+		
+		$xop=$xop.'<result ';
+		$patt_link='#<a href="/title/(.*)/"#U';
+		preg_match($patt_link,$pieces[$i],$link_value);
+		$xop=$xop.'details="http://imdb.com/title/'.$link_value[1].'"';
+		
+		
+		
+		$patt_rating='#<span class="value">(...)</span>#Us';
+		$ret_check=preg_match($patt_rating,$pieces[$i],$rating_value);
+		if($ret_check==0)
+		{
+			$xop=$xop.' rating="-" ';
+		}
+		else
+		$xop=$xop.' rating="'.$rating_value[1].'" ';
+		
+		
+		$patt_dir='#Dir: <a href=".*">(.*)</a>#Us';
+		preg_match($patt_dir,$pieces[$i],$dir_value);
+		if($dir_value==NULL)
+			$xop=$xop.'director=N.A. ';
+		else
+			$xop=$xop.'director="'.$dir_value[1].'" ';
+		
+		$patt_year='#<span class="year_type">\((....).*\)</span>#U';
+		preg_match($patt_year,$pieces[$i],$year_value);
+		if($year_value==NULL || $year_value[1]=="????")
+			$xop=$xop.'year=N.A. ';
+		else
+			$xop=$xop.'year="'.$year_value[1].'" ';
+		
+		$patt_title='#title="(.*)\(.*\)"#U';
+		preg_match($patt_title,$pieces[$i],$title_value);
+		$xop=$xop.'title="'.$title_value[1].'" ';
+		
+		$patt_img='#<img src="(.*)"#U';
+		preg_match($patt_img,$pieces[$i],$img_value);
+		$xop=$xop.'cover="'.$img_value[1].'" ';
+		
+		$xop=$xop.'/>';
+		
+	}	
+		$xop=$xop.'</rsp>';
+		$xop=$xop.'</results>';
+		
+	
+}
+	echo $xop;
+?>
---
+++ hw8.java
@@ -0,0 +1,115 @@
+import java.io.*;
+import java.io.StringReader;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.net.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+
+
+
+
+
+  public class hw8 extends HttpServlet {
+
+
+    public void doGet(HttpServletRequest request,HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        
+        response.setContentType("application/json; charset:utf-8");
+        request.setCharacterEncoding("UTF-8");
+		
+		String title_val=request.getParameter("title");
+		String tt_val=request.getParameter("title_type");
+		JSONObject j_results = new JSONObject();
+		
+		PrintWriter out1 = response.getWriter();
+	 try {	
+		String value1= request.getParameter("title");
+		String value2= request.getParameter("title_type");
+		
+		
+	//	 WORKING JUST FINE  EXPECT MARY 
+			String data = URLEncoder.encode("title", "UTF-8") + "=" + URLEncoder.encode(value1, "UTF-8");
+        data += "&" + URLEncoder.encode("title_type", "UTF-8") + "=" + URLEncoder.encode(value2, "UTF-8");
+		String url_php="http://cs-server.usc.edu:20212/hw8p.php?"+data;
+
+        URL url = new URL(url_php);
+        URLConnection conn = url.openConnection();
+		conn.setAllowUserInteraction(false);
+        conn.setDoOutput(true);
+       
+	   
+	   
+	    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+        String line1,line2="";
+        while ((line1 = rd.readLine()) != null)
+		{
+			line2+=line1;
+        
+        }
+		
+		InputSource is = new InputSource();
+		is.setCharacterStream(new StringReader(line2));
+		
+		JSONObject j_result = new JSONObject();
+		
+		
+	
+			
+			DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			Document doc = db.parse(is);
+			NodeList games = doc.getElementsByTagName("result");
+			
+			for (int i = 0; i < games.getLength(); i++) 
+			{
+			  Node aNode = games.item(i);
+			  NamedNodeMap attributes = aNode.getAttributes();
+				
+			JSONObject j_inresult = new JSONObject();
+			  for (int a = 0; a < attributes.getLength(); a++) 
+			  {
+				
+				Node theAttribute = attributes.item(a);
+				
+				j_inresult.put(theAttribute.getNodeName(),theAttribute.getNodeValue());
+				//System.out.println(theAttribute.getNodeName() + "=" + theAttribute.getNodeValue());
+			  }
+			  j_result.accumulate("result",j_inresult);
+			}
+			j_results.put("results",j_result);
+			
+		}
+		catch(MalformedURLException m)
+		{
+			out1.println("mexp");
+			return;
+		}
+		catch(IOException ioo)
+		{
+			out1.println("ioexp");
+			return;
+		
+		}
+		catch(Exception e)
+		{
+			out1.println("error");
+			return;
+		}
+		out1.println( j_results.toString(2) );
+
+		
+    }
+}
